f873dd3cf0ca5f9d51fd4398dcb0bcd61b31b4c0
Karen Carter <kcarter@nmda.nmsu.edu>
2018-03-23 13:21:49 -0600

    Fixed simulator point order and got very initial version of animation working.

--- death_star_animation.py
+++ death_star_animation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Light each LED in sequence, and repeat.
+# Adapted from fadecandy's chase.py
 
 import opc, time
 
@@ -8,8 +8,17 @@ numLEDs = 390
 client = opc.Client('127.0.0.1:22368')
 
 while True:
-	for i in range(numLEDs):
-		pixels = [ (0,0,0) ] * numLEDs
-		pixels[i] = (255, 255, 255)
-		client.put_pixels(pixels)
-		time.sleep(0.05)
+    for i in range(30):
+        pixels = [ (0,0,0) ] * numLEDs
+        for j in range(6):
+            pixels[ (j*30) + i] = (0, 255, 0)
+            #print("i: ", i)
+            #print("j: ", j)
+        client.put_pixels(pixels)
+        time.sleep(0.05)
+
+    for i in range(210):
+        pixels = [ (0,0,0) ] * numLEDs
+        pixels[i + 180] = (255, 255, 255)
+        client.put_pixels(pixels)
+        time.sleep(0.05)
--- simulator_points_init.rb
+++ simulator_points_init.rb
@@ -8,16 +8,17 @@ r = 2.0
 spacing = 14.0 / 210.0
 spacing_2 = z / 30.0
 
+(0..5).each do |i|
+  (0..29).each do |j|
+    radius = r - (j * spacing_2)
+    points << [radius * Math.cos((i * Math::PI) / 3.0 ), radius * Math.sin((i * Math::PI) / 3.0), j * spacing_2]
+  end
+end
+
 (0..209).each do |i|
   points << [x, y, i * spacing + z]
 end
 
-(0..29).each do |j|
-  radius = r - (j * spacing_2)
-  (0..5).each do |i|
-    points << [radius * Math.cos((i * Math::PI) / 3.0 ), radius * Math.sin((i * Math::PI) / 3.0), j * spacing_2]
-  end
-end
 
 puts "["
 points[0...-1].each do |point|
