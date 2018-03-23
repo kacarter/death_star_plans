11df510583259028125654df8181fe22e324edf1
Karen Carter <kcarter@nmda.nmsu.edu>
2018-03-23 17:18:56 -0600

    Starting on getting the dashed beams to work.

--- death_star_animation.py
+++ death_star_animation.py
@@ -8,17 +8,19 @@ numLEDs = 390
 client = opc.Client('127.0.0.1:22368')
 
 while True:
+    pixels = [ (0,0,0) ] * numLEDs
     for i in range(30):
-        pixels = [ (0,0,0) ] * numLEDs
         for j in range(6):
             pixels[ (j*30) + i] = (0, 255, 0)
             #print("i: ", i)
             #print("j: ", j)
+            if (i % 3 == 0):
+                pixels[ (j*30) + i - 3] = (0, 0, 0)
         client.put_pixels(pixels)
-        time.sleep(0.05)
+        time.sleep(0.15)
 
     for i in range(210):
         pixels = [ (0,0,0) ] * numLEDs
-        pixels[i + 180] = (255, 255, 255)
+        pixels[i + 180] = (0, 255, 0)
         client.put_pixels(pixels)
-        time.sleep(0.05)
+        time.sleep(0.02)
