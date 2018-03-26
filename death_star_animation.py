130d1cf3c796eb2d134856085abed84ef96f9859
Karen Carter <kcarter@nmda.nmsu.edu>
2018-03-26 17:05:00 -0600

    Has dashed lines that continue past the initial filling up of each short strip.

--- death_star_animation.py
+++ death_star_animation.py
@@ -8,16 +8,25 @@ numLEDs = 390
 client = opc.Client('127.0.0.1:22368')
 
 while True:
-    pixels = [ (0,0,0) ] * numLEDs
+    pixels = [(0,0,0)] * numLEDs
     for i in range(30):
         for j in range(6):
-            pixels[ (j*30) + i] = (0, 255, 0)
-            #print("i: ", i)
-            #print("j: ", j)
-            if (i % 3 == 0):
-                pixels[ (j*30) + i - 3] = (0, 0, 0)
+            for k in range(i):
+                pixels[(j*30) + k] = (0, 150, 0)
+                if (k % 4 == i % 4):
+                    pixels[(j*30) + k] = (0, 0, 0)
         client.put_pixels(pixels)
         time.sleep(0.15)
+
+    for i in range(30):
+        for j in range(6):
+            for k in range(30):
+                pixels[(j*30) + k] = (0, 150, 0)
+                if (k % 4 == i % 4):
+                    pixels[(j*30) + k] = (0, 0, 0)
+        client.put_pixels(pixels)
+        time.sleep(0.15)
+
 
     for i in range(210):
         pixels = [ (0,0,0) ] * numLEDs
