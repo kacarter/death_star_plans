563b487359591697cc6a7518709f70192c0a81d9
Karen Carter <kcarter@nmda.nmsu.edu>
2019-01-17 10:26:38 -0700

    Added animations for Christmas and saying goodbye to the amazing wonderful Death Star.

---
+++ christmas_animation.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+# Adapted from fadecandy's chase.py
+
+import threading, time, opc, serial, cv2, random
+from playsound import playsound
+
+client = opc.Client('127.0.0.1:7890')
+# client = opc.Client('127.0.0.1:22368')
+numLEDs = 512
+pixels = [(0, 0, 0)] * numLEDs
+background_pixels = 192
+background_counter = 0
+num_spokes = 5
+strip_length = 64
+spoke_length = 30
+pixel_off = (0, 0, 0)
+animation_speed = 0.15
+colors = [(0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 255, 0), (255, 0, 255), (255, 165, 0)]
+
+
+def background_animation():
+    global background_pixels
+    global background_counter
+    global pixels
+    global pixel_off
+    for b in range(background_pixels):
+        if b % 6 in [background_counter, (background_counter + 1) % 6, (background_counter + 2) % 6]:
+            pixels[b + (num_spokes * strip_length)] = (0, 0, 255)
+        if b % 6 in [(background_counter + 3) % 6, (background_counter + 4) % 6, (background_counter + 5) % 6]:
+            pixels[b + (num_spokes * strip_length)] = pixel_off
+    background_counter = (background_counter + 1) % 6
+
+
+def play_animation():
+    global pixels
+    global client
+    global numLEDs
+    global num_spokes
+    global strip_length
+    global pixel_off
+    global animation_speed
+    global spoke_length
+    laser_length = 30
+
+    pixels = [pixel_off] * numLEDs
+    for i in range(spoke_length):
+        for j in range(num_spokes):
+            for k in range(i):
+                pixels[(j * strip_length) + k] = (0, 150, 0)
+                if k % 6 in [i % 6, (i + 1) % 6, (i + 2) % 6]:
+                    pixels[(j * strip_length) + k] = (150, 0, 0)
+        background_animation()
+        client.put_pixels(pixels)
+        time.sleep(animation_speed)
+
+    for i in range(spoke_length):
+        for j in range(num_spokes):
+            for k in range(spoke_length):
+                if i >= 10 and k >= 10:
+                    extra_brightness = (((i - 10) % 20) * ((k - 10) % 20)) / 4
+                    pixels[(j * strip_length) + k] = (extra_brightness * 1.5, 164 + extra_brightness, extra_brightness * 1.5)
+                    if k % 6 in [i % 6, (i + 1) % 6, (i + 2) % 6]:
+                        pixels[(j * strip_length) + k] = (164 + extra_brightness, extra_brightness * 1.5, extra_brightness * 1.5)
+                else:
+                    pixels[(j * strip_length) + k] = (0, 150, 0)
+                    if k % 6 in [i % 6, (i + 1) % 6, (i + 2) % 6]:
+                        pixels[(j * strip_length) + k] = (150, 0, 0)
+        background_animation()
+        client.put_pixels(pixels)
+        time.sleep(animation_speed)
+
+    for i in range(strip_length - spoke_length):
+        pixels[i + ((num_spokes - 1) * strip_length) + spoke_length] = random.choice(colors)
+        if i <= laser_length + spoke_length:
+            for j in range(num_spokes):
+                for k in range(spoke_length):
+                    if (k >= 10):
+                        extra_brightness = (19 * ((k - 10) % 20)) / 4
+                        pixels[(j * strip_length) + k] = (extra_brightness * 1.5, 164 + extra_brightness, extra_brightness * 1.5)
+                        if k % 6 in [i % 6, (i + 1) % 6, (i + 2) % 6]:
+                            pixels[(j * strip_length) + k] = (
+                            164 + extra_brightness, extra_brightness * 1.5, extra_brightness * 1.5)
+                    else:
+                        pixels[(j * strip_length) + k] = (0, 150, 0)
+                        if k % 6 in [i % 6, (i + 1) % 6, (i + 2) % 6]:
+                            pixels[(j * strip_length) + k] = (150, 0, 0)
+                if (i > laser_length - spoke_length):
+                    for k in range(i - laser_length + spoke_length):
+                        pixels[(j * strip_length) + k] = pixel_off
+        if i >= laser_length:
+            pixels[i - laser_length + ((num_spokes - 1) * strip_length) + spoke_length] = pixel_off
+        background_animation()
+        client.put_pixels(pixels)
+        time.sleep(animation_speed)
+
+    for i in range(laser_length):
+        pixels[i + (num_spokes * strip_length) - laser_length] = pixel_off
+        background_animation()
+
+        client.put_pixels(pixels)
+        time.sleep(animation_speed)
+
+    pixels = [pixel_off] * numLEDs
+    client.put_pixels(pixels)
+
+
+try:
+    serial_port = serial.Serial('/dev/ttyUSB1', 9600)
+except:
+    serial_port = serial.Serial('/dev/ttyUSB0', 9600)
+sound = "death_star.mp3"
+button_error = False
+
+while True:
+    try:
+        if button_error == True:
+            try:
+                serial_port = serial.Serial('/dev/ttyUSB1', 9600)
+                button_error = False
+            except:
+                try:
+                    serial_port = serial.Serial('/dev/ttyUSB0', 9600)
+                    button_error = False
+                except:
+                    button_error = True
+        button_status = serial_port.readline().decode().strip('\r\n')
+    except:
+        print("button error")
+        button_error = True
+        time.sleep(300)
+        pass
+    # print(button_status)
+    if (button_status == '1'):
+        t = threading.Thread(target=play_animation)
+        t.daemon = True
+        t.start()
+        try:
+            playsound(sound)
+        except:
+            print("Can't play audio")
+        try:
+            serial_port.reset_input_buffer()
+        except:
+            pass
+
